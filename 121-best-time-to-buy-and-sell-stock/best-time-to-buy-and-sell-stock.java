3c81d73c3198465e1b813bfbe1c5fa3b8cb9133c
gagan m suvarna <gagan2suvarna@gmail.com>
2026-08-26 21:47:35 +0530

    Time: 2 ms (44.00%) | Memory: 94.5 MB (51.56%) - LeetSync

---
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int minprices=prices[0];
+        int maxprofit=0;
+        for(int i=1;i<prices.length;i++){
+            if(prices[i]<minprices){
+                minprices=prices[i];
+            }
+            int profit=prices[i]-minprices;
+            if (maxprofit<profit){
+                maxprofit=profit;
+            }
+
+        }
+        return maxprofit;
+
+    }
+}
